0e97b9a1c3cf1c5c1b7d2c875bd3549bad1b077a
Iervin2 <iervin@usca.edu>
2021-11-10 13:17:13 -0500

    Finished Nov10th

--- js/hotel-json.js
+++ js/hotel-json.js
@@ -1,33 +1,37 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+const firebaseConfig = {
+  apiKey: 'AIzaSyBrqis-cT9SY8Dn13a9wWdy0zAoYIjVlz0',
+  authDomain: 'csci225-iervin.firebaseapp.com',
+  projectId: 'csci225-iervin',
+  storageBucket: 'csci225-iervin.appspot.com',
+  messagingSenderId: '917846296557',
+  appId: '1:917846296557:web:c6c73fca789db695d8c9e4',
+  measurementId: 'G-SM68Q4DXW9',
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-/* object examples 
+//object examples
 var testJson = {};
-testJson["lastname"] = "zhang";
-testJson["location"] = "aiken";
+testJson['lastname'] = 'zhang';
+testJson['location'] = 'aiken';
 console.log(testJson);
-*/
 
 // enter data in
-$("input[type='button']").click(function(e) {
+$("input[type='button']").click(function (e) {
   //get the value of form
-  
-  
+  const input = $('form').serializeArray();
+  console.log(input);
   /* save the data to database */
-
-
+  var inputJSON = {};
+  for (var i = 0; i < input.length; i++) {
+    var n = input[i]['name'];
+    var v = input[i]['value'];
+    inputJSON[n] = v;
+  }
+  firebase.firestore().collection('hotelRes').add(inputJSON); //saves the data
 
   /* clear the entry */
-  $("form")[0].reset();
+  $('form')[0].reset();
 });
 
 /* array example
@@ -39,12 +43,15 @@ array1.forEach(element => console.log(element));
 
 firebase
   .firestore()
-  .collection("hoteldata")
+  .collection("hotelRes")
   .onSnapshot(querySnapshot => {
     console.log(querySnapshot.size);
     querySnapshot.forEach(doc => {
       console.log(doc.data());
+      console.log(doc.data().name);
+      console.log(doc.data().num);
       console.log(doc.data().room);
+      console.log(doc.data().checkin)
       console.log(doc.data().checkout);
     });
   });
